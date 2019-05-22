bacb80964b6ee04eb56f452a4c5af31ca6e24493
“2016012075xieyueyue <2456739803@qq.com>
2019-05-22 22:36:09 +0800

    aaaaa

---
+++ User.java
@@ -0,0 +1,39 @@
+package com.nenu.soft.bolg.bean;
+
+public class User {
+	
+	private String id;
+	private String name;
+	private String loginName;
+	private String psw;
+	
+	
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getLoginName() {
+		return loginName;
+	}
+	public void setLoginName(String loginName) {
+		this.loginName = loginName;
+	}
+	public String getPsw() {
+		return psw;
+	}
+	public void setPsw(String psw) {
+		this.psw = psw;
+	}
+	
+	
+	
+
+}
